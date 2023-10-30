1b82f554b0e52b2156d68407ee9470e6e2a256fd
AKASH DEY <83662743+Akash-msit@users.noreply.github.com>
2023-10-30 19:15:51 +0000

    DataType

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+let name = "Akash";
+let age = 23
+let isLoggedIn = false
+console.log(typeof(isLoggedIn));
+
+//premitive datatype
+
+/*
+    number
+    string => ""
+    boolean => true/false
+    bigint
+    null => standalon value
+    undefined
+    symbol => unique
+*/
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)  //object
